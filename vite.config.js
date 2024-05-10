b77753b2b23c904dd8d4f7b9b7ecdfdc42e2b7a9
mbgame <mohsen.bashirzadeh@gmail.com>
2024-05-10 21:59:38 +0330

    remove textures folder inside planet folder

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     root: 'src/',
     publicDir: '../public/', 
-    // base: 'https://mbgame.github.io/planet/', 
+    base: 'https://mbgame.github.io/planet/', 
     build: {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true,
-        // assetPrefix: '/planet/' 
+        assetPrefix: '/planet/' 
     },
 });
